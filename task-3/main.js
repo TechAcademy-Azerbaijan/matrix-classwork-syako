15d678327d759bf571a27d39f09fa45b982e4308
Sarkhan <syako44@gmail.com>
2022-07-04 02:47:24 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,4 +1,19 @@
 // Use this array for solution
 let array = [3, 5, -7, 7, 5, -9, -4]
 
-// Write code here. 
+let arr2 = [];
+let count = 0
+
+for(i = 0 ; i < arr.length ; i++ ){
+    for(j = 0 ; j < arr.length ; j++ ){
+
+        if(arr[i]==arr[j]){
+            count++
+        }
+    }
+    if(count===1){
+        arr2.push(arr[i])
+    }
+    count = 0
+}
+console.log(arr2);
