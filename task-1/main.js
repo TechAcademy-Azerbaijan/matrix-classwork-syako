8b79086c0d5c5aa238b70f74841d2bfa9d19797b
Sarkhan <syako44@gmail.com>
2022-07-04 02:45:29 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -2,4 +2,14 @@
 let array_1 = [3, 1, 3, 4, 2, 4, 12]
 let array_2 = [4, 15, 43, 1, 15, 1]
 
-// Write code here. 
+let arr3 = [];
+
+for(i = 0 ; i < arr1.length ; i++){
+
+    if(!arr2.includes(arr1[i])){
+        arr3.push(arr1[i])
+    }
+
+}
+console.log(arr3.length);
+console.log(arr3.join(" "));
