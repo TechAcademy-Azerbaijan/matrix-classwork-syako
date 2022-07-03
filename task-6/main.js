5d768169098f0a6a0f609663a40f5b5d9b738f58
Sarkhan <syako44@gmail.com>
2022-07-04 02:51:23 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -12,4 +12,14 @@ let matrix_2 = [
     [1, 0, 1]
 ]; 
 
-// Write code here. 
+sum = 0
+
+for( i = 0 ; i < matrix2.length ; i++ ){
+
+    for( k = 0 ; k < matrix2[i].length ; k++ ){
+
+        if(matrix2[i][k] == 1){
+            sum = sum + matrix1[i][k]
+        }
+    }
+}console.log(sum);
