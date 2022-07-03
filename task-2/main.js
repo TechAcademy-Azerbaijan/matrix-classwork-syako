c2b1a6878c0f60cde30677e52097a6695089b259
Sarkhan <syako44@gmail.com>
2022-07-04 02:45:59 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,4 +1,12 @@
 // Use this array for solution
 let array = [3, 5, -7, 7, 5, -9, -4]
 
-// Write code here. 
+let arr2 = [];
+
+for(let i = 0 ; i < arr.length ; i++){
+
+    if(!arr2.includes(arr[i])){
+        arr2.push(arr[i])
+    }
+
+}console.log(arr2.length);
